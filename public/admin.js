e4af4f1fedabdb2e19f9503e7f699c5f7c62caaf
Bret Doucette <doucette.bret@gmail.com>
2016-01-14 11:40:07 -0700

    General cleanup

--- public/admin.js
+++ public/admin.js
@@ -1,27 +1,12 @@
 var socket = io();
 
 socket.on('voteCount', function(message) {
-  console.log(message)
   $('.vote-count').empty();
   for (var key in message){
-    $('.vote-count').append("Poll Item: " + key + " has " + message[key] +  " vote(s).<br>");
-}
-
-// for (var k in target){
-//     if (target.hasOwnProperty(k)) {
-//          alert("Key is " + k + ", value is" + target[k]);
-//     }
-// }
-
-  // debugger
-  // .forOwn(message, function(value, key) {
-  //   debugger
-  //   $('.vote-count').append(`${key}`);
-  // } );
-//   for (var key in message) {
-//     debugger
-
-//   // console.log(key);
-// }
-  // console.log(message)
+    $('.vote-count').append("Poll Item: "
+                            + key
+                            + " has "
+                            + message[key]
+                            +  " vote(s).<br>");
+  }
 });
