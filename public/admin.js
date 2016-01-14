f0c0a8be76008a8e18dcb51d9e52c35d363f5838
Bret Doucette <doucette.bret@gmail.com>
2016-01-14 11:46:08 -0700

    Refactored admin.js

--- public/admin.js
+++ public/admin.js
@@ -2,6 +2,15 @@ var socket = io();
 
 socket.on('voteCount', function(message) {
   $('.vote-count').empty();
+  hideNofiticiation();
+  displayVotes(message);
+});
+
+function hideNofiticiation () {
+  $('p').hide();
+}
+
+function displayVotes (message) {
   for (var key in message){
     $('.vote-count').append("Poll Item: "
                             + key
@@ -9,4 +18,4 @@ socket.on('voteCount', function(message) {
                             + message[key]
                             +  " vote(s).<br>");
   }
-});
+}
