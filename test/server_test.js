5536ae0a50534043ceb9864195732ab3c88d12f6
Bret Doucette <doucette.bret@gmail.com>
2016-01-16 14:32:05 -0700

    Added additional testing for routes

--- test/server_test.js
+++ test/server_test.js
@@ -10,10 +10,34 @@ describe('server.js', function () {
       .get('/')
       .expect(200, done);
   });
-  it('GET "/dontgohere" should return a 404 response', function (done) {
+
+  it('GET "/badroute" should return a 404 response', function (done) {
     request(server)
-      .get('/dontgohere')
+      .get('/badroute')
       .expect(404, done);
+  });
+
+  it('should give a 404 on random routes', function (done) {
+    request(server)
+      .get('/this/is/a/random/routes')
+      .expect(404, done);
+  });
+
+  it('should give you a 404 if you try to post to random routes', function (done) {
+    request(server)
+      .post('/this/is/a/random/route')
+      .expect(404, done);
+  });
+
+  it('should create a poll and redirect upon sucessful post', function (done) {
+    request(server)
+      .post('/')
+      .type('form')
+      .send( { items: ['A', 'B'] } )
+      .end(function(error, response){
+        expect(response.header['location']).to.include('/admin/');
+        done();
+      })
   });
 });
 
