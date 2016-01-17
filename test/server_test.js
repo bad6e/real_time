db69e360c2bb735bc3543b7f6586279f4e513321
Bret Doucette <doucette.bret@gmail.com>
2016-01-16 17:36:50 -0700

    Added tests for polls/:id and polls/admin/:id

--- test/server_test.js
+++ test/server_test.js
@@ -3,12 +3,42 @@ var chai = require('chai');
 var expect = chai.expect;
 var request = require('supertest');
 var server = require('../server.js');
+const PollStorage = require('../lib/pollstorage');
 
 describe('server.js', function () {
+
   it('GET "/" should return a 200 response', function (done) {
     request(server)
       .get('/')
       .expect(200, done);
+  });
+
+  it('GET "/polls/:id" should return a 200 response', function (done) {
+    request(server)
+      .post('/')
+      .type('form')
+      .send( { items: ['A', 'B'] } )
+      .end(function(error, response){
+        expect(response.header['location']).to.include('/admin/');
+        var uniqueKey = response.header.location.split('/').slice(-1).pop();
+        request(server)
+         .get('/polls/'+ uniqueKey)
+         .expect(200, done);
+        });
+  });
+
+  it('GET "/polls/admin/:id" should return a 200 response', function (done) {
+    request(server)
+      .post('/')
+      .type('form')
+      .send( { items: ['A', 'B'] } )
+      .end(function(error, response){
+        expect(response.header['location']).to.include('/admin/');
+        var uniqueKey = response.header.location.split('/').slice(-1).pop();
+        request(server)
+         .get('/polls/admin/'+ uniqueKey)
+         .expect(200, done);
+        });
   });
 
   it('GET "/badroute" should return a 404 response', function (done) {
@@ -17,19 +47,19 @@ describe('server.js', function () {
       .expect(404, done);
   });
 
-  it('should give a 404 on random routes', function (done) {
+  it('should give a 404 on a GET request to random routes', function (done) {
     request(server)
       .get('/this/is/a/random/routes')
       .expect(404, done);
   });
 
-  it('should give you a 404 if you try to post to random routes', function (done) {
+  it('should give you a 404 if you try to POST to random routes', function (done) {
     request(server)
       .post('/this/is/a/random/route')
       .expect(404, done);
   });
 
-  it('should create a poll and redirect upon sucessful post', function (done) {
+  it('should create a poll and redirect upon sucessful POST', function (done) {
     request(server)
       .post('/')
       .type('form')
@@ -37,7 +67,7 @@ describe('server.js', function () {
       .end(function(error, response){
         expect(response.header['location']).to.include('/admin/');
         done();
-      })
+    })
   });
 });
 
