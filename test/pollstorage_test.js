ace3b6de162f35442599c6208e49c384a17c010e
Bret Doucette <doucette.bret@gmail.com>
2016-01-18 14:55:14 -0700

    Cleaned up tests

--- test/pollstorage_test.js
+++ test/pollstorage_test.js
@@ -9,10 +9,8 @@ describe('PollStorage Functions', function () {
     this.message = { items: [ 'A', 'B' ] };
     this.voteMessage = { key: 'cd1917c7', value: 'A' };
     this.socketId = 'Qdna#ds';
-
     this.response = {
       render: function() {
-        //Render a mock page page
       }
     }
   });
@@ -21,13 +19,13 @@ describe('PollStorage Functions', function () {
     expect(this.pollStorage.polls).eql({});
   });
 
-  it('creates a poll with a poll key as the key and the poll items as the value', function (){
+  it('creates a poll with a poll key as the key and the poll items as the value', function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll(this.message, key);
     expect(this.pollStorage.polls).eql({abc123: { items: [ 'A', 'B' ] } });
   });
 
-  it('checks if a vote has been assigned to a specific poll - if no then call the function to assign it', function (){
+  it('checks if a vote has been assigned to a specific poll - if no then call the function to assign it', function () {
     var votes = {};
     var theSpy = sinon.spy(this.pollStorage, "assignVotesToPoll");
     var checkingPoll = this.pollStorage.checkIfVotesHaveBeenAssignedToPoll(this.voteMessage, votes, this.socketId);
@@ -35,7 +33,7 @@ describe('PollStorage Functions', function () {
     theSpy.restore();
   });
 
-  it('checks if a vote has been assigned to a specific poll - if yes then call the function add votes to existing polls', function (){
+  it('checks if a vote has been assigned to a specific poll - if yes then call the function add votes to existing polls', function () {
     var votes = { cd1917c7: { 'Zdna#ds': 'A' } };
     var theSpy = sinon.spy(this.pollStorage, "addVotesToExistingPoll");
     var checkingPoll = this.pollStorage.checkIfVotesHaveBeenAssignedToPoll(this.voteMessage, votes, this.socketId);
@@ -50,9 +48,9 @@ describe('PollStorage Functions', function () {
   });
 
   it('added votes to a poll if the poll has already been assigned', function () {
-    var votes = { cd1917c7: { 'Xdna#ds': 'A' } }
+    var votes = { cd1917c7: { 'Xdna#ds': 'A' } };
     var assignedPoll = this.pollStorage.addVotesToExistingPoll(this.voteMessage, votes, this.socketId );
-    expect(votes).eql({ cd1917c7: { 'Xdna#ds': 'A', 'Qdna#ds': 'A' } })
+    expect(votes).eql({ cd1917c7: { 'Xdna#ds': 'A', 'Qdna#ds': 'A' } });
   });
 
   it('counts the votes of a each poll', function (){
@@ -62,7 +60,7 @@ describe('PollStorage Functions', function () {
     expect(votesTally).eql({ cd1917c7: { A: 1 } });
   });
 
-  it('counts the votes of a each poll when there are multiple unique polls and multiple votes', function (){
+  it('counts the votes of a each poll when there are multiple unique polls and multiple votes', function () {
     var votesTally = {};
     var votes = { cd1917c7: { 'Qdna#ds': 'A' },
                   rd191dc7: { 'Adna#df': 'B', 'Sdna#df': 'B' } };
@@ -71,7 +69,7 @@ describe('PollStorage Functions', function () {
                              rd191dc7: { B: 2 }});
   });
 
-  it('filters the votes by the poll key when there are multiple polls with multiple votes', function (){
+  it('filters the votes by the poll key when there are multiple polls with multiple votes', function () {
     var votesTally = { 'cd1917c7': { A: 2 }, '237cc8f': { C: 1 } };
     var filteredPoll = this.pollStorage.fiterVotesByMessageKey(this.voteMessage, votesTally);
     expect(filteredPoll).eql({ A: 2 });
@@ -81,10 +79,8 @@ describe('PollStorage Functions', function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll({ items: [ 'A', 'C' ], share: 'true' }, key);
     var endedPoll = this.pollStorage.endPoll(key);
-
     var theSpy = sinon.spy(this.response, "render")
     var checkingPoll = this.pollStorage.checkIfPollClosed(key, this.response);
-
     sinon.assert.calledOnce(theSpy);
     theSpy.restore();
   });
@@ -102,9 +98,8 @@ describe('PollStorage Functions', function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll({ items: [ 'A', 'C' ], endtime: '14:35' }, key);
     var timeStub = sinon.stub(this.pollStorage, "getTimeInCorrectForm").returns('14:35');
-
-    var endPollSpy = sinon.stub(this.pollStorage, "endPoll")
-    var responseSpy = sinon.spy(this.response, "render")
+    var endPollSpy = sinon.stub(this.pollStorage, "endPoll");
+    var responseSpy = sinon.spy(this.response, "render");
     var endedPoll = this.pollStorage.checkIfPollTimeIsOver(key, this.response)
     sinon.assert.calledOnce(endPollSpy);
     sinon.assert.calledOnce(responseSpy);
@@ -116,83 +111,68 @@ describe('PollStorage Functions', function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll({ items: [ 'A', 'C' ], endtime: '14:32' }, key);
     var timeStub = sinon.stub(this.pollStorage, "getTimeInCorrectForm").returns('14:35');
-
-    var endPollSpy = sinon.spy(this.pollStorage, "endPoll")
-    var responseSpy = sinon.spy(this.response, "render")
-
-    var endedPoll = this.pollStorage.checkIfPollTimeIsOver(key, this.response)
+    var endPollSpy = sinon.spy(this.pollStorage, "endPoll");
+    var responseSpy = sinon.spy(this.response, "render");
+    var endedPoll = this.pollStorage.checkIfPollTimeIsOver(key, this.response);
     sinon.assert.notCalled(endPollSpy);
     sinon.assert.calledOnce(responseSpy);
-
     endPollSpy.restore();
     responseSpy.restore();
-
   });
 
   it("checks to see if the poll is shared - if yes it obtains a poll's status and renders the poll page with a true status", function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll({ items: [ 'A', 'C' ], share: 'true' }, key);
-
-    var pollStatusSpy = sinon.spy(this.pollStorage, "obtainPollStatus")
-    var responseSpy = sinon.spy(this.response, "render")
-
-    var endedPoll = this.pollStorage.checkIfPollIsShared(key, this.response)
-
+    var pollStatusSpy = sinon.spy(this.pollStorage, "obtainPollStatus");
+    var responseSpy = sinon.spy(this.response, "render");
+    var endedPoll = this.pollStorage.checkIfPollIsShared(key, this.response);
     sinon.assert.calledOnce(pollStatusSpy);
     sinon.assert.calledOnce(responseSpy);
-
     pollStatusSpy.restore();
     responseSpy.restore();
-
   });
 
   it('checks to see if the poll is shared - if no it renders the poll page', function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll({ items: [ 'A', 'C' ]}, key);
-
-    var pollStatusSpy = sinon.spy(this.pollStorage, "obtainPollStatus")
-    var responseSpy = sinon.spy(this.response, "render")
-
-    var endedPoll = this.pollStorage.checkIfPollIsShared(key, this.response)
-
+    var pollStatusSpy = sinon.spy(this.pollStorage, "obtainPollStatus");
+    var responseSpy = sinon.spy(this.response, "render");
+    var endedPoll = this.pollStorage.checkIfPollIsShared(key, this.response);
     sinon.assert.notCalled(pollStatusSpy);
     sinon.assert.calledOnce(responseSpy);
-
     pollStatusSpy.restore();
     responseSpy.restore();
   });
 
-  it('ends a specific poll based on a poll key when there are multiple polls', function (){
+  it('ends a specific poll based on a poll key when there are multiple polls', function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll(this.message, key);
-
     var keyTwo = "cde456";
     var newPollTwo = this.pollStorage.createPoll({ items: [ 'C', 'D' ] }, keyTwo);
-
     var endedPoll = this.pollStorage.endPoll(key);
     expect(this.pollStorage.polls).eql({ abc123: null, cde456: { items: [ 'C', 'D' ] } });
   });
 
-  it("it obtains a poll's items - the poll's questions", function (){
+  it("it obtains a poll's items - the poll's questions", function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll(this.message, key);
     var pollItems = this.pollStorage.obtainPollItems('abc123');
     expect(pollItems).eql([ 'A', 'B' ]);
   });
 
-  it("it obtains a poll's status if that status is true", function (){
+  it("it obtains a poll's status if that status is true", function () {
     var key = "abc123";
     var newPoll = this.pollStorage.createPoll({ items: [ 'A', 'C' ], share: 'true' }, key);
     var pollStatus = this.pollStorage.obtainPollStatus('abc123');
     expect(pollStatus).eql('true');
   });
 
-  it("get the time in the correct format as a string", function (){
+  it("get the time in the correct format as a string", function () {
     var pollTime = this.pollStorage.getTimeInCorrectForm();
     assert.isString(pollTime, 'must be a string');
   });
 
-  it("get the time in the correct format with a length of five - including : ", function (){
+  it("get the time in the correct format with a length of five - including : ", function () {
     var pollTime = this.pollStorage.getTimeInCorrectForm();
     expect(pollTime.length).eql(5);
   });
